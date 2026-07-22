1487087148f270637097d85ed281d7e67fedb116
Daria <121836323+GavrishD@users.noreply.github.com>
2026-07-22 18:18:23 +0300

    Add JavaScript basics homework

---
+++ git_basics/jsBasics/task3.js
@@ -0,0 +1,13 @@
+//Task 3: Numbers and Booleans
+
+const firstUserAge = 15;
+const secondUserAge = 25;
+
+const isFirstUserAdult = firstUserAge >= 18;
+const isSecondUserAdult = secondUserAge >= 18;
+
+console.log("First user's age:", firstUserAge);
+console.log("Is the first user an adult?", isFirstUserAdult);
+
+console.log("Second user's age:", secondUserAge);
+console.log("Is the second user an adult?", isSecondUserAdult);
